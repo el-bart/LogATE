3a8dc78baf784b065192ca3b4a102aebca1ac3f8
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-03-28 11:51:59 +0100

    reorderign Node's structure (WIP)

--- src/LogATE/Tree/Node.hpp
+++ src/LogATE/Tree/Node.hpp
@@ -10,31 +10,43 @@ namespace LogATE::Tree
 class Node
 {
 public:
-  struct Name final
+  enum class CompareElement
   {
-    std::string value_;
+      Key,
+      Value
   };
+  struct Name final { std::string value_; };
+
+  virtual ~Node() = default;
+
+  Node(Node const&) = delete;
+  Node const& operator=(Node const&) = delete;
+  Node(Node&&) = delete;
+  Node const& operator=(Node&&) = delete;
 
   virtual Name name() const = 0;
   virtual Path processedField() const = 0;
 
-  SequenceNumber first() const;
-  SequenceNumber last() const;
+  Log first() const;
+  Log last() const;
 
   SequenceNumber insert(Log log) const;
 
   // pruneUpTo(SeqN)
   std::vector<Log> range(SequenceNumber begin, SequenceNumber end) const;
-  std::vector<Log> range(SequenceNumber begin, size_t count) const;
-  std::vector<But::NotNullShared<Node>> children() const;
+  std::vector<Log> from(SequenceNumber first, size_t count) const;
+  std::vector<Log> to(SequenceNumber last, size_t count) const;
+  std::vector<But::NotNullRaw<Node>> children() const;
 
 protected:
-
+  Node(Name filterName, Path path);
 
 private:
+  virtual bool matchesFilter(Log const& log) = 0;
+
   mutable std::mutex mutex_;
   std::vector<Log> logs_;
-  std::vector<But::NotNullShared<Node>> children_;
+  std::vector<But::NotNullUnique<Node>> children_;
 };
 
 }
