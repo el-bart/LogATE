6698ec1a31cdde4f97a4077558ad5ccc2e0cc06d
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-03-29 16:49:45 +0100

    added begin()/end() to Path

--- src/LogATE/Tree/Path.hpp
+++ src/LogATE/Tree/Path.hpp
@@ -8,6 +8,8 @@ namespace LogATE::Tree
 struct Path final
 {
   auto root() const { return value_.empty() || value_[0] == "."; }
+  auto begin() const { return value_.begin(); }
+  auto end() const { return value_.end(); }
 
   std::vector<std::string> value_;
 };
