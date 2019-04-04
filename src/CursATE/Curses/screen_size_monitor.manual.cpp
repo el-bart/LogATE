641a05645f9c7b12e058b9459c60ff79eb6cf815
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-04-04 21:51:31 +0200

    removed default sdtscr from c-tor

--- src/CursATE/Curses/screen_size_monitor.manual.cpp
+++ src/CursATE/Curses/screen_size_monitor.manual.cpp
@@ -7,10 +7,10 @@ int main()
   const CursATE::Curses::Init init;
   for(auto i=0; ; ++i)
   {
-    CursATE::Curses::ScreenSize ss;
+    CursATE::Curses::ScreenSize ss{stdscr};
     mvprintw(0, 0, "iteration %d: screen is %d rows and %d cols", i, ss.rows_.value_, ss.columns_.value_);
 
-    CursATE::Curses::ScreenPosition sp;
+    CursATE::Curses::ScreenPosition sp{stdscr};
     mvprintw(1, 0, "iteration %d: last cursor position was %d row and %d col", i, sp.row_.value_, sp.column_.value_);
 
     refresh();
