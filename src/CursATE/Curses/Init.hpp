bcdeb976c6ee3d63014e4c978d63fc8651871b97
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-04-04 22:13:49 +0200

    enabling colors by default

--- src/CursATE/Curses/Init.hpp
+++ src/CursATE/Curses/Init.hpp
@@ -1,14 +1,20 @@
 #pragma once
 #include <ncurses.h>
+#include "CursATE/Curses/Exception.hpp"
 
 namespace CursATE::Curses
 {
 
 struct Init
 {
+  BUT_DEFINE_EXCEPTION(ColorsNotSupported, Exception, "colors not supported");
+
   Init()
   {
     initscr();  // ncurses init
+    if(not has_colors())
+      BUT_THROW(ColorsNotSupported, "has_colors() returned false");
+    start_color();
     cbreak();   // ^Z and ^C shall generate signals
     noecho();
     keypad(stdscr, TRUE);   // enable funciton keys, arrows, etc...
