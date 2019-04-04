db3e951c882ae5bb321a98d3192ed92be3ba291f
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-04-04 15:20:12 +0200

    added more corner cases for socket handling

--- src/LogATE/Net/TcpServer.it.cpp
+++ src/LogATE/Net/TcpServer.it.cpp
@@ -1,12 +1,14 @@
 #include <doctest/doctest.h>
 #include "LogATE/Net/TcpServer.hpp"
 #include "LogATE/Net/TcpClient.hpp"
+#include "LogATE/Net/TcpRawClient.hpp"
 #include "LogATE/TestPrints.ut.hpp"
 
 using LogATE::Log;
 using LogATE::Net::Port;
 using LogATE::Net::TcpServer;
 using LogATE::Net::TcpClient;
+using LogATE::Net::TcpRawClient;
 
 namespace
 {
@@ -32,9 +34,11 @@ TEST_CASE_FIXTURE(Fixture, "server can handle one client")
 {
   TcpServer s{port_};
   TcpClient c{host_, port_};
+  CHECK( s.errors() == 0 );
 
   c.write(log1_);
   const auto log = s.readNextLog();
+  CHECK( s.errors() == 0 );
   REQUIRE(log);
   CHECK( *log->log_ == log1_ );
 }
@@ -48,8 +52,75 @@ TEST_CASE_FIXTURE(Fixture, "server can handle one client and multiple messages")
   {
     c.write(json);
     const auto log = s.readNextLog();
+    CHECK( s.errors() == 0 );
     REQUIRE(log);
     CHECK( *log->log_ == json );
+  }
+}
+
+TEST_CASE_FIXTURE(Fixture, "server does not disconnect on parse errors")
+{
+  TcpServer s{port_};
+  TcpRawClient rc{host_, port_};
+
+  CHECK( s.errors() == 0 );
+  rc.write("not a valid json\n");
+  rc.write( log1_.dump() );
+
+  const auto log = s.readNextLog();
+  CHECK( s.errors() == 12 );
+  REQUIRE(log);
+  CHECK( *log->log_ == log1_ );
+}
+
+TEST_CASE_FIXTURE(Fixture, "server can handle json spread through multiple lines")
+{
+  TcpServer s{port_};
+  TcpRawClient rc{host_, port_};
+
+  const auto jsonStr = std::string{R"( {
+                                         "one": 1,
+                                         "two": []
+                                       })"};
+  rc.write(jsonStr);
+
+  const auto log = s.readNextLog();
+  CHECK( s.errors() == 0 );
+  REQUIRE(log);
+  CHECK( *log->log_ == nlohmann::json::parse(jsonStr) );
+}
+
+TEST_CASE_FIXTURE(Fixture, "server can handle jsons attached to next other")
+{
+  TcpServer s{port_};
+  TcpRawClient rc{host_, port_};
+
+  const auto jsonStr = std::string{R"({"foo":"bar"})"};
+  const auto count = 2;
+  for(auto i=0; i<count; ++i)
+    rc.write(jsonStr);
+
+  for(auto i=0; i<count; ++i)
+  {
+    const auto log = s.readNextLog();
+    CHECK( s.errors() == 0 );
+    REQUIRE(log);
+    CHECK( *log->log_ == nlohmann::json::parse(jsonStr) );
+  }
+}
+
+TEST_CASE_FIXTURE(Fixture, "server gets back to accepting connections once current client disconnects")
+{
+  TcpServer s{port_};
+
+  for(auto i=0; i<2; ++i)
+  {
+    TcpClient c{host_, port_};
+    c.write(log1_);
+    const auto log = s.readNextLog();
+    CHECK( s.errors() == i );
+    REQUIRE(log);
+    CHECK( *log->log_ == log1_ );
   }
 }
 
