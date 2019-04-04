c5cd90a3b9118e9b1563f415739f4f0352f39aab
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-04-04 23:40:40 +0200

    enum defining ncurses colors in a type-safe fashion

---
+++ src/CursATE/Curses/Colors.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <ncurses.h>
+
+namespace CursATE::Curses
+{
+
+enum class Color
+{
+  Black  = COLOR_BLACK,
+  Red    = COLOR_RED,
+  Green  = COLOR_GREEN,
+  Yellow = COLOR_YELLOW,
+  Blue   = COLOR_BLUE,
+  Magent = COLOR_MAGENTA,
+  Cyan   = COLOR_CYAN,
+  White  = COLOR_WHITE
+};
+
+}
