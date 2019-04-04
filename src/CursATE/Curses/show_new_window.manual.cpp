bb80d14c199a7524e0f9303434fc310156eb27dd
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-04-04 22:06:43 +0200

    added printing of screen size and position

--- src/CursATE/Curses/show_new_window.manual.cpp
+++ src/CursATE/Curses/show_new_window.manual.cpp
@@ -6,7 +6,18 @@ using namespace CursATE::Curses;
 int main()
 {
   const Init init;
-  Window win{ ScreenPosition{Row{2}, Column{10}}, ScreenSize{Rows{5}, Columns{20}}, Window::Boxed::True };
-  win.refresh();
-  getch();
+  Window win{ ScreenPosition{Row{2}, Column{10}}, ScreenSize{Rows{5}, Columns{60}}, Window::Boxed::True };
+
+  for(auto i=0; ; ++i)
+  {
+    CursATE::Curses::ScreenSize ss{win.window_};
+    mvwprintw(win.window_, 1, 1, "iteration %d: screen is %d rows and %d cols", i, ss.rows_.value_, ss.columns_.value_);
+
+    CursATE::Curses::ScreenPosition sp{win.window_};
+    mvwprintw(win.window_, 2, 1, "iteration %d: last cursor position was %d row and %d col", i, sp.row_.value_, sp.column_.value_);
+
+    win.refresh();
+    if( getch() == 'q' )
+      break;
+  }
 }
