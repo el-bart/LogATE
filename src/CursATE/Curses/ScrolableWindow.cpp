50ab70a2414af2d49f1c365e1fb2e5cbd4935fb1
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-04-12 15:16:15 +0200

    added selection highligh

--- src/CursATE/Curses/ScrolableWindow.cpp
+++ src/CursATE/Curses/ScrolableWindow.cpp
@@ -7,19 +7,22 @@ void ScrolableWindow::refresh()
 {
   const auto uas = window_.userAreaSize();
   backend_.resize(uas);
-  //backend_.update();
   const auto displayData = backend_.displayData();
   BUT_ASSERT( displayData.lines_.size() <= static_cast<size_t>(uas.rows_.value_) );
 
   const auto uap = window_.userAreaStartPosition();
   for(auto i=0u; i<displayData.lines_.size(); ++i)
   {
-    if( displayData.lines_[i].first == displayData.currentSelection_ )
-    {
-      // TODO: add higlight for a selected item.
-    }
+    const auto isSelected = displayData.lines_[i].first == displayData.currentSelection_;
+    const auto markAttr = A_REVERSE;
+    if(isSelected)
+      wattr_on( window_.get(), markAttr, nullptr);
+
     mvwprintw( window_.get(), uap.row_.value_+i, uap.column_.value_, "%s", displayData.lines_[i].second.c_str() );
-    clrtoeol();
+    wclrtoeol( window_.get() );
+
+    if(isSelected)
+      wattr_off( window_.get(), markAttr, nullptr);
   }
 
   window_.refresh();
