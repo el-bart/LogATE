52e370cf61937f486726c05f5be64c7b43894de0
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-04-15 13:22:22 +0200

    map of nodes-to-open-windows

---
+++ src/CursATE/detail/FilterWindows.cpp
@@ -0,0 +1,41 @@
+#include "CursATE/detail/FilterWindows.hpp"
+#include "CursATE/detail/LogDataSource.hpp"
+
+namespace CursATE::detail
+{
+
+FilterWindows::FilterWindows(std::function<std::string(LogATE::Log const&)> log2str):
+  log2str_{ std::move(log2str) }
+{ }
+
+
+But::NotNullShared<Curses::ScrolableWindow> FilterWindows::window(LogATE::Tree::NodeShPtr const& node)
+{
+  auto f = [&](auto const& e) { return e.node_.lock().get() == node.get(); };
+  const auto it = std::find_if( begin(entries_), end(entries_), f );
+  if( it != end(entries_) )
+    return it->win_;
+  const auto win = newWindow(node);
+  entries_.push_back( Element{ node.underlyingPointer(), win } );
+  return win;
+}
+
+
+void FilterWindows::prune()
+{
+  auto f = [&](auto const& e) { return e.node_.lock().get() == nullptr; };
+  const auto it = std::remove_if( begin(entries_), end(entries_), f );
+  entries_.erase(it, end(entries_));
+}
+
+
+But::NotNullShared<Curses::ScrolableWindow> FilterWindows::newWindow(LogATE::Tree::NodeShPtr node) const
+{
+  auto ds = But::makeSharedNN<LogDataSource>( std::move(node), log2str_ );
+  const auto sp = Curses::ScreenPosition{Curses::Row{0}, Curses::Column{0}};
+  const auto ss = Curses::ScreenSize::global();
+  const auto boxed = Curses::Window::Boxed::True;
+  return But::makeSharedNN<Curses::ScrolableWindow>(ds, sp, ss, boxed);
+}
+
+}
---
+++ src/CursATE/detail/FilterWindows.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "LogATE/Tree/Node.hpp"
+#include "CursATE/Curses/DataSource.hpp"
+#include "CursATE/Curses/ScrolableWindow.hpp"
+#include <functional>
+#include <vector>
+
+namespace CursATE::detail
+{
+
+class FilterWindows
+{
+public:
+  explicit FilterWindows(std::function<std::string(LogATE::Log const&)> log2str);
+
+  But::NotNullShared<Curses::ScrolableWindow> window(LogATE::Tree::NodeShPtr const& node);
+  void prune();
+
+private:
+  But::NotNullShared<Curses::ScrolableWindow> newWindow(LogATE::Tree::NodeShPtr node) const;
+
+  struct Element
+  {
+    LogATE::Tree::NodeWeakPtr node_;
+    But::NotNullShared<Curses::ScrolableWindow> win_;
+  };
+  std::vector<Element> entries_;
+
+  const std::function<std::string(LogATE::Log const&)> log2str_;
+};
+
+}
