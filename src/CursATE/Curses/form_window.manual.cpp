05fdea13ed06596b303d9a23afe281bdfd890964
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-04-16 15:15:27 +0200

    helper function for processing form

--- src/CursATE/Curses/form_window.manual.cpp
+++ src/CursATE/Curses/form_window.manual.cpp
@@ -4,26 +4,27 @@
 
 using namespace CursATE::Curses;
 
-int main()
+auto processForm()
 {
-  const Init init;
-  set(CursorVisibility::Invisible);
   auto form = makeForm( Field::Input{"txt"},
                         Field::Radio{"radio", {"foo/bar", "narf"}},
                         Field::Button{"ok"},
                         Field::Button{"cancel"} );
 
-  do
-  {
-    const auto res = form.process();
-    clear();
-    refresh();
+  form.refresh();
+  return form.process();
+}
 
-    mvwprintw( stdscr, 1, 1, "user input:      %s", res[0].c_str() );
-    mvwprintw( stdscr, 2, 1, "radio selection: %s", res[1].c_str() );
-    mvwprintw( stdscr, 3, 1, "OK button:       %s", res[2].c_str() );
-    mvwprintw( stdscr, 3, 1, "cancel button:   %s", res[3].c_str() );
-    refresh();
-  }
-  while( getch() != 'q' );
+int main()
+{
+  const Init init;
+  set(CursorVisibility::Invisible);
+  const auto res = processForm();
+
+  mvwprintw( stdscr, 1, 1, "user input:      %s", res[0].c_str() );
+  mvwprintw( stdscr, 2, 1, "radio selection: %s", res[1].c_str() );
+  mvwprintw( stdscr, 3, 1, "OK button:       %s", res[2].c_str() );
+  mvwprintw( stdscr, 3, 1, "cancel button:   %s", res[3].c_str() );
+  refresh();
+  getch();
 }
