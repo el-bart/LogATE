e426a5bb288938e15a1042b8b23a5dcc53a3a569
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-04-17 11:25:09 +0200

    added helper function to calculate all the offsets

--- src/CursATE/Curses/Field/detail/resizePadded.cpp
+++ src/CursATE/Curses/Field/detail/resizePadded.cpp
@@ -3,14 +3,10 @@
 namespace CursATE::Curses::Field::detail
 {
 
-std::string resizePadded(std::string const& in, const size_t maxSize, const size_t selectedElement)
+VisibleSize resizePaddedVisibleSize(std::string const& in, size_t maxSize, size_t selectedElement)
 {
   if( in.size() <= maxSize )
-  {
-    auto tmp = in;
-    tmp.resize(maxSize, ' ');
-    return tmp;
-  }
+    return {0, selectedElement, in.size()};
   if( selectedElement >= in.size() )
     BUT_THROW(SelectionOutOfRange, "requested element " << selectedElement << " in a string of length " << in.size());
 
@@ -20,7 +16,20 @@ std::string resizePadded(std::string const& in, const size_t maxSize, const size
     start = selectedElement - half;
   if( in.size() - start < maxSize )
     start = in.size() - maxSize;
-  return in.substr(start, maxSize);
+  const auto offset = in.size() - start;
+  return {start, offset, maxSize};
+}
+
+std::string resizePadded(std::string const& in, const size_t maxSize, const size_t selectedElement)
+{
+  if( in.size() <= maxSize )
+  {
+    auto tmp = in;
+    tmp.resize(maxSize, ' ');
+    return tmp;
+  }
+  const auto vs = resizePaddedVisibleSize(in, maxSize, selectedElement);
+  return in.substr(vs.start_, vs.count_);
 }
 
 }
--- src/CursATE/Curses/Field/detail/resizePadded.hpp
+++ src/CursATE/Curses/Field/detail/resizePadded.hpp
@@ -7,6 +7,14 @@ namespace CursATE::Curses::Field::detail
 
 BUT_DEFINE_EXCEPTION(SelectionOutOfRange, Exception, "selection out of range");
 
+struct VisibleSize
+{
+  size_t start_{0};
+  size_t selectionOffset_{0};
+  size_t count_{0};
+};
+
+VisibleSize resizePaddedVisibleSize(std::string const& in, size_t maxSize, size_t selectedElement);
 std::string resizePadded(std::string const& in, size_t maxSize, size_t selectedElement);
 
 }
