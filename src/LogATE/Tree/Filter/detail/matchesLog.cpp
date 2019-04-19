0ba4fa0f7b78481fdeb021a6617d893cf3a0ba67
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-04-19 16:28:35 +0200

    added explicit differenciation to convert number that are integers to an integer, not double

--- src/LogATE/Tree/Filter/detail/matchesLog.cpp
+++ src/LogATE/Tree/Filter/detail/matchesLog.cpp
@@ -36,7 +36,9 @@ But::Optional<std::string> value2str(nlohmann::json const& node)
 {
   if( node.is_string() )
     return node.get<std::string>();
-  if( node.is_number() )
+  if( node.is_number_integer() )
+    return std::to_string( node.get<int64_t>() );
+  if( node.is_number_float() )
     return std::to_string( node.get<double>() );
   if( node.is_boolean() )
     return std::string{ node.get<bool>() ? "true" : "false" };
