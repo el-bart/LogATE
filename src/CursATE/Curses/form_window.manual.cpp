a260c1066cc8410b7e8f58acf986c6919340d7bf
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-04-26 22:07:13 +0200

    one more text field

--- src/CursATE/Curses/form_window.manual.cpp
+++ src/CursATE/Curses/form_window.manual.cpp
@@ -8,7 +8,9 @@ auto processForm()
 {
   auto form = makeForm( KeyShortcuts{
                           {'i', "txt"},
-                          {'I', "txt2"},
+                          {'1', "txt"},
+                          {'2', "txt2"},
+                          {'3', "txt3"},
                           {'r', "radio"},
                           {'o', "ok"},
                           {'q', "cancel"},
@@ -16,6 +18,7 @@ auto processForm()
                         },
                         Field::Input{"txt", "default window text XXX AAA"},
                         Field::Input{"txt2", "other stuff"},
+                        Field::Input{"txt3", "and one more"},
                         Field::Radio{"radio", {"foo/bar", "narf"}},
                         Field::Button{"ok"},
                         Field::Button{"cancel"} );
@@ -30,10 +33,11 @@ int main()
   const auto res = processForm();
 
   mvwprintw( stdscr, 1, 1, "user input:      %s", res[0].c_str() );
-  mvwprintw( stdscr, 1, 1, "user input 2:    %s", res[1].c_str() );
-  mvwprintw( stdscr, 2, 1, "radio selection: %s", res[2].c_str() );
-  mvwprintw( stdscr, 3, 1, "OK button:       %s", res[3].c_str() );
-  mvwprintw( stdscr, 4, 1, "cancel button:   %s", res[4].c_str() );
+  mvwprintw( stdscr, 2, 1, "user input 2:    %s", res[1].c_str() );
+  mvwprintw( stdscr, 3, 1, "user input 3:    %s", res[2].c_str() );
+  mvwprintw( stdscr, 4, 1, "radio selection: %s", res[3].c_str() );
+  mvwprintw( stdscr, 5, 1, "OK button:       %s", res[4].c_str() );
+  mvwprintw( stdscr, 6, 1, "cancel button:   %s", res[5].c_str() );
   refresh();
   getch();
 }
