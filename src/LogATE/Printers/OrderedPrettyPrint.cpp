32b59ef853912310db746b872936be780ae33f0e
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-05-06 11:10:53 +0200

    added sequence number to log print

--- src/LogATE/Printers/OrderedPrettyPrint.cpp
+++ src/LogATE/Printers/OrderedPrettyPrint.cpp
@@ -24,6 +24,7 @@ OrderedPrettyPrint::OrderedPrettyPrint(PriorityTags const& priorityTags, SilentT
 std::string OrderedPrettyPrint::operator()(LogATE::Log const& in) const
 {
   std::stringstream ss;
+  ss << in.sn_.value_ << " ";
   constructString(ss, *in.log_);
   return ss.str();
 }
--- src/LogATE/Printers/OrderedPrettyPrint.ut.cpp
+++ src/LogATE/Printers/OrderedPrettyPrint.ut.cpp
@@ -9,35 +9,41 @@ namespace
 TEST_SUITE("LogATE::Printers::OrderedPrettyPrint")
 {
 
+auto mklog(const unsigned sn, std::string const& json)
+{
+  auto log = json2log(json);
+  log.sn_.value_ = sn;
+  return log;
+}
 
 TEST_CASE("directly converting to pretty print")
 {
   const OrderedPrettyPrint opp_;
   SUBCASE("basic types")
   {
-    CHECK( "mystring=xxx" == opp_( json2log(R"({"mystring": "xxx"})") ) );
-    CHECK( "myint=42" == opp_( json2log(R"({"myint":42})") ) );
-    CHECK( "mybigint=1234567890" == opp_( json2log(R"({"mybigint":1234567890})") ) );
-    CHECK( "myfloat=4.5" == opp_( json2log(R"({"myfloat":4.5})") ) );
-    CHECK( "mysmallfloat=0.0005" == opp_( json2log(R"({"mysmallfloat":0.0005})") ) );
-    CHECK( "mybigfloat=123456789.5" == opp_( json2log(R"({"mybigfloat":123456789.5})") ) );
-    CHECK( "mybool=true" == opp_( json2log(R"({"mybool":true})") ) );
+    CHECK( "13 mystring=xxx" == opp_( mklog(13, R"({"mystring": "xxx"})") ) );
+    CHECK( "13 myint=42" == opp_( mklog(13, R"({"myint":42})") ) );
+    CHECK( "13 mybigint=1234567890" == opp_( mklog(13, R"({"mybigint":1234567890})") ) );
+    CHECK( "13 myfloat=4.5" == opp_( mklog(13, R"({"myfloat":4.5})") ) );
+    CHECK( "13 mysmallfloat=0.0005" == opp_( mklog(13, R"({"mysmallfloat":0.0005})") ) );
+    CHECK( "13 mybigfloat=123456789.5" == opp_( mklog(13, R"({"mybigfloat":123456789.5})") ) );
+    CHECK( "13 mybool=true" == opp_( mklog(13, R"({"mybool":true})") ) );
   }
   SUBCASE("basic structures")
   {
-    CHECK( "" == opp_( json2log(R"({})") ) );
-    CHECK( "foo=bar" == opp_( json2log(R"({"foo":"bar"})") ) );
-    const auto dualFields = opp_( json2log(R"({"foo":"bar", "answer":42})") );
-    if( "foo=bar answer=42" != dualFields && "answer=42 foo=bar" != dualFields )
+    CHECK( "13 " == opp_( mklog(13, R"({})") ) );
+    CHECK( "13 foo=bar" == opp_( mklog(13, R"({"foo":"bar"})") ) );
+    const auto dualFields = opp_( mklog(13, R"({"foo":"bar", "answer":42})") );
+    if( "13 foo=bar answer=42" != dualFields && "13 answer=42 foo=bar" != dualFields )
     {
-      CHECK( "foo=bar answer=42" == dualFields );
-      CHECK( "answer=42 foo=bar" == dualFields );
+      CHECK( "13 foo=bar answer=42" == dualFields );
+      CHECK( "13 answer=42 foo=bar" == dualFields );
     }
   }
   SUBCASE("nested structures")
   {
-    CHECK( "answer={ expected=42 }" == opp_( json2log(R"({"answer":{ "expected":42 }})") ) );
-    CHECK( "narf={ fran={ xxx=42 } }" == opp_( json2log(R"({"narf": { "fran": { "xxx":42 } } })") ) );
+    CHECK( "13 answer={ expected=42 }" == opp_( mklog(13, R"({"answer":{ "expected":42 }})") ) );
+    CHECK( "13 narf={ fran={ xxx=42 } }" == opp_( mklog(13, R"({"narf": { "fran": { "xxx":42 } } })") ) );
   }
 }
 
@@ -47,17 +53,17 @@ TEST_CASE("converting with priorities")
   const OrderedPrettyPrint opp_{ OrderedPrettyPrint::PriorityTags{{"foo", "bar"}} };
   SUBCASE("root elements")
   {
-    CHECK( "narf=xxx" == opp_( json2log(R"({"narf": "xxx"})") ) );
-    CHECK( "foo=xxx narf=yyy" == opp_( json2log(R"({"narf": "yyy", "foo":"xxx"})") ) );
-    CHECK( "bar=xxx narf=yyy" == opp_( json2log(R"({"narf": "yyy", "bar":"xxx"})") ) );
-    CHECK( "foo=xxx bar=zzz narf=yyy" == opp_( json2log(R"({"narf": "yyy", "bar":"zzz", "foo":"xxx"})") ) );
+    CHECK( "13 narf=xxx" == opp_( mklog(13, R"({"narf": "xxx"})") ) );
+    CHECK( "13 foo=xxx narf=yyy" == opp_( mklog(13, R"({"narf": "yyy", "foo":"xxx"})") ) );
+    CHECK( "13 bar=xxx narf=yyy" == opp_( mklog(13, R"({"narf": "yyy", "bar":"xxx"})") ) );
+    CHECK( "13 foo=xxx bar=zzz narf=yyy" == opp_( mklog(13, R"({"narf": "yyy", "bar":"zzz", "foo":"xxx"})") ) );
   }
   SUBCASE("nested elements tags")
   {
-    CHECK( "root={ narf=xxx }" == opp_( json2log(R"({"root": {"narf": "xxx"} })") ) );
-    CHECK( "root={ foo=xxx narf=yyy }" == opp_( json2log(R"({"root": {"narf": "yyy", "foo":"xxx"} })") ) );
-    CHECK( "root={ bar=xxx narf=yyy }" == opp_( json2log(R"({"root": {"narf": "yyy", "bar":"xxx"} })") ) );
-    CHECK( "root={ foo=xxx bar=zzz narf=yyy }" == opp_( json2log(R"({"root": {"narf": "yyy", "bar":"zzz", "foo":"xxx"} })") ) );
+    CHECK( "13 root={ narf=xxx }" == opp_( mklog(13, R"({"root": {"narf": "xxx"} })") ) );
+    CHECK( "13 root={ foo=xxx narf=yyy }" == opp_( mklog(13, R"({"root": {"narf": "yyy", "foo":"xxx"} })") ) );
+    CHECK( "13 root={ bar=xxx narf=yyy }" == opp_( mklog(13, R"({"root": {"narf": "yyy", "bar":"xxx"} })") ) );
+    CHECK( "13 root={ foo=xxx bar=zzz narf=yyy }" == opp_( mklog(13, R"({"root": {"narf": "yyy", "bar":"zzz", "foo":"xxx"} })") ) );
   }
 }
 
@@ -67,16 +73,16 @@ TEST_CASE("silent tags")
   const OrderedPrettyPrint opp_{ OrderedPrettyPrint::SilentTags{{"foo", "bar"}} };
   SUBCASE("root elements")
   {
-    CHECK( "narf=xxx" == opp_( json2log(R"({"narf": "xxx"})") ) );
-    CHECK( "xxx" == opp_( json2log(R"({"bar": "xxx"})") ) );
-    CHECK( "xxx" == opp_( json2log(R"({"foo": "xxx"})") ) );
+    CHECK( "13 narf=xxx" == opp_( mklog(13, R"({"narf": "xxx"})") ) );
+    CHECK( "13 xxx" == opp_( mklog(13, R"({"bar": "xxx"})") ) );
+    CHECK( "13 xxx" == opp_( mklog(13, R"({"foo": "xxx"})") ) );
   }
   SUBCASE("nested elements tags")
   {
-    CHECK( "root={ narf=xxx }" == opp_( json2log(R"({"root": {"narf": "xxx"} })") ) );
-    CHECK( "root={ xxx }" == opp_( json2log(R"({"root": {"bar": "xxx"} })") ) );
-    CHECK( "root={ xxx }" == opp_( json2log(R"({"root": {"foo": "xxx"} })") ) );
-    CHECK( "{ narf=xxx }" == opp_( json2log(R"({"bar": {"narf": "xxx"} })") ) );
+    CHECK( "13 root={ narf=xxx }" == opp_( mklog(13, R"({"root": {"narf": "xxx"} })") ) );
+    CHECK( "13 root={ xxx }" == opp_( mklog(13, R"({"root": {"bar": "xxx"} })") ) );
+    CHECK( "13 root={ xxx }" == opp_( mklog(13, R"({"root": {"foo": "xxx"} })") ) );
+    CHECK( "13 { narf=xxx }" == opp_( mklog(13, R"({"bar": {"narf": "xxx"} })") ) );
   }
 }
 
@@ -84,7 +90,7 @@ TEST_CASE("silent tags")
 TEST_CASE("silent tags with priorities")
 {
   const OrderedPrettyPrint opp_{ OrderedPrettyPrint::SilentTags{{"foo", "bar"}}, OrderedPrettyPrint::PriorityTags{{"foo", "narf"}} };
-  CHECK( "xxx narf=yyy zzz" == opp_( json2log(R"({"narf": "yyy", "bar":"zzz", "foo":"xxx"})") ) );
+  CHECK( "13 xxx narf=yyy zzz" == opp_( mklog(13, R"({"narf": "yyy", "bar":"zzz", "foo":"xxx"})") ) );
 }
 
 }
