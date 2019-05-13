96291b77ff3e4be8a39771031202cdbcee1100b6
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-05-13 14:32:15 +0200

    Window is nwo split into CPP/HPP

--- src/CursATE/Curses/Window.hpp
+++ src/CursATE/Curses/Window.hpp
@@ -18,27 +18,8 @@ struct Window
     True
   };
 
-  Window(const ScreenPosition sp, const ScreenSize ss, const Boxed boxed):
-    boxed_{boxed},
-    window_{ newwin(ss.rows_.value_, ss.columns_.value_,
-                    sp.row_.value_,  sp.column_.value_) }
-  {
-    if(not window_)
-      BUT_THROW(FailedToCreateWindow, "nullptr received");
-    keypad(window_, TRUE);   // enable funciton keys, arrows, etc...
-  }
-
-  ~Window()
-  {
-    switch(boxed_)
-    {
-      case Boxed::True: wborder(window_, ' ', ' ', ' ',' ',' ',' ',' ',' '); break;
-      case Boxed::False: break;
-    }
-    clear();
-    wrefresh(window_);
-    delwin(window_);
-  }
+  Window(ScreenPosition sp, ScreenSize ss, Boxed boxed);
+  ~Window();
 
   Window(Window const&) = delete;
   Window& operator=(Window const&) = delete;
@@ -46,46 +27,18 @@ struct Window
   Window(Window&&) = delete;
   Window& operator=(Window&&) = delete;
 
-  void clear() const
-  {
-    wclear(window_);
-  }
-
-  void refresh() const
-  {
-    switch(boxed_)
-    {
-      case Boxed::True: box(window_, 0, 0); break;
-      case Boxed::False: break;
-    }
-    wrefresh(window_);
-  }
+  void clear() const;
+  void refresh() const;
 
   auto size() const { return ScreenSize{window_}; }
   auto position() const { return ScreenPosition{window_}; }
   auto boxed() const { return boxed_ == Boxed::True; }
   auto get() const { return window_; }
 
-  auto userAreaStartPosition() const
-  {
-    if( boxed() )
-      return ScreenPosition{ Row{1}, Column{1} };
-    return ScreenPosition{ Row{0}, Column{0} };
-  }
-  auto userAreaSize() const
-  {
-    const auto ss = size();
-    if( boxed() )
-      return ScreenSize{ Rows{std::max(ss.rows_.value_-2, 0)}, Columns{std::max(ss.columns_.value_-2, 0)} };
-    return ss;
-  }
+  ScreenPosition userAreaStartPosition() const;
+  ScreenSize userAreaSize() const;
 
-  void colors(const Color background, const Color font)
-  {
-    const auto cp = 1;
-    init_pair( cp, static_cast<int>(font), static_cast<int>(background) );
-    wbkgd(window_, COLOR_PAIR(cp));
-  }
+  void colors(Color background, Color font);
 
 private:
   const Boxed boxed_;
