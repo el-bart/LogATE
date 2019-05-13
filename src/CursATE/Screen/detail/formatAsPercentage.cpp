115f2c887b4b95ee12ae7773bafff3b60f449ff2
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-05-13 09:41:36 +0200

    left-padding with spaces in now a separate function

--- src/CursATE/Screen/detail/formatAsPercentage.cpp
+++ src/CursATE/Screen/detail/formatAsPercentage.cpp
@@ -19,10 +19,18 @@ std::string formatAsPercentage(const double value)
   str.erase(it, str.end());
 
   // add leading spaces for 100.00% to fit exactly
-  if( pos < 3 )
-    str.insert(0, 3-pos, ' ');
+  return padLeftWithSpaces( std::move(str), sizeof("100.00")-1 ) + '%';
+}
 
-  return std::move(str) + '%';
+
+std::string padLeftWithSpaces(std::string str, const size_t maxLen)
+{
+  if( str.size() >= maxLen )
+    return str;
+  const auto missstrg = maxLen - str.size();
+  const auto space = ' ';
+  str.insert(0, missstrg, space);
+  return str;
 }
 
 }
--- src/CursATE/Screen/detail/formatAsPercentage.hpp
+++ src/CursATE/Screen/detail/formatAsPercentage.hpp
@@ -7,4 +7,5 @@ namespace CursATE::Screen::detail
 BUT_DEFINE_EXCEPTION(NegativeValuesNotSupported, But::Exception, "negative values are not supported");
 
 std::string formatAsPercentage(const double value);
+std::string padLeftWithSpaces(std::string str, size_t maxLen);
 }
