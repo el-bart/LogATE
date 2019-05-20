9b2bed8c41a396225c2927311364a4fd10e39b01
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-05-20 17:52:19 +0200

    WIP of line splitter

--- src/CursATE/Screen/detail/splitIntoLines.cpp
+++ src/CursATE/Screen/detail/splitIntoLines.cpp
@@ -1,20 +1,96 @@
 #include "CursATE/Screen/detail/splitIntoLines.hpp"
+#include "LogATE/Utils/PrintableStringConverter.hpp"
+#include <But/assert.hpp>
 #include <sstream>
+#include <iostream>                 
+#include <algorithm>
 
 
 namespace CursATE::Screen::detail
 {
 
+namespace
+{
+auto isEscape(const char c) { return c == '\\'; }
+auto isCr(const char c) { return c == 'n'; }
+auto isLf(const char c) { return c == 'r'; }
+auto isEolChar(const char c) { return isCr(c) || isLf(c); }
+
+auto hasEscaped(char const c, const std::string::const_iterator begin, const std::string::const_iterator end)
+{
+  if(begin==end)
+    return false;
+  if( not isEscape(*begin) )
+    return false;
+  const auto it = begin + 1;
+  BUT_ASSERT(it!=end && "invalid sequence provided - break should not be in the middle of an escape sequence");
+  return *it==c;
+}
+auto hasEscapedCr(const std::string::const_iterator begin, const std::string::const_iterator end) { return hasEscaped('n', begin, end); }
+auto hasEscapedLf(const std::string::const_iterator begin, const std::string::const_iterator end) { return hasEscaped('r', begin, end); }
+
+auto findFirstEol(const std::string::const_iterator begin, const std::string::const_iterator end)
+{
+  for(auto it=begin; it!=end; ++it)
+  {
+    if( hasEscapedCr(it, end) )
+    {
+      if( hasEscapedLf(it+2, end) )
+        return it+4;
+      return it+2;
+    }
+    if( hasEscapedCr(it, end) )
+    {
+      if( hasEscapedLf(it+2, end) )
+        return it+4;
+      return it+2;
+    }
+    if( isEscape(*it) )
+    {
+      ++it;
+      BUT_ASSERT(it!=end && "invalid sequence - cannot end with broken escape sequence");
+    }
+  }
+  return end;
+}
+
+auto findLastInLineBeforeSplitEscape(const std::string::const_iterator begin, const std::string::const_iterator end, const size_t cols)
+{
+  const auto inRange = static_cast<size_t>( std::distance(begin, end) );
+  const auto effectiveEnd = ( inRange >= cols ) ? begin+cols : end;
+  for(auto it=begin; it!=effectiveEnd; ++it)
+  {
+    if( not isEscape(*it) )
+      continue;
+    if(it+1 == effectiveEnd)
+      return it;
+    ++it;
+  }
+  return effectiveEnd;
+}
+
+auto findNextLineBreak(const std::string::const_iterator begin, const std::string::const_iterator end, const size_t cols)
+{
+  const auto newEnd = findLastInLineBeforeSplitEscape(begin, end, cols);
+  std::cout << "\nEE:|" << std::string{begin, newEnd} << "| of |" << std::string{begin, end} << "| @ " << cols << " cols" << std::endl;         
+  return findFirstEol(begin, newEnd);
+}
+}
+
 std::vector<std::string> splitIntoLines(std::string const& in, const size_t availableColumns)
 {
-  const auto columns = std::max<size_t>(80u/2u, availableColumns);  // if needed, horizontal scroll will do the trick here...
-  (void) columns;           
+  if( in.empty() )
+    return {};
+  if( not std::all_of( begin(in), end(in), isprint ) )
+    std::logic_error{"detail::splitIntoLines(): input sequence shall already be escaped"};
+  const auto columns = std::max<size_t>(1u, availableColumns);
   std::vector<std::string> out;
   auto it = begin(in);
   while( it != end(in) )
   {
-    break;
-    // TODO...
+    const auto lb = findNextLineBreak(it, end(in), columns);
+    out.emplace_back(it, lb);
+    it = lb;
   }
   return out;
 }
--- src/CursATE/Screen/detail/splitIntoLines.ut.cpp
+++ src/CursATE/Screen/detail/splitIntoLines.ut.cpp
@@ -16,57 +16,178 @@ TEST_CASE("empty string")
 }
 
 
-TEST_CASE("")
+TEST_CASE("single line")
 {
-  // TODO
+  const auto out = splitIntoLines("foo bar", 100);
+  REQUIRE( out.size() == 1u );
+  CHECK( out[0] == "foo bar" );
 }
 
 
-TEST_CASE("")
+TEST_CASE("dwline")
 {
-  // TODO
+  const auto out = splitIntoLines("foo bar", 100);
+  REQUIRE( out.size() == 1u );
+  CHECK( out[0] == "foo bar" );
 }
 
 
-TEST_CASE("")
+TEST_CASE("wrapping lines")
 {
-  // TODO
+  SUBCASE("exactly the column size does not break line")
+  {
+    const auto out = splitIntoLines("foobar", 6);
+    REQUIRE( out.size() == 1u );
+    CHECK( out[0] == "foobar" );
+  }
+  SUBCASE("over line lenght break the line")
+  {
+    const auto out = splitIntoLines("foobarX", 6);
+    REQUIRE( out.size() == 2u );
+    CHECK( out[0] == "foobar" );
+    CHECK( out[1] == "X" );
+  }
+  SUBCASE("round multi-lines")
+  {
+    const auto out = splitIntoLines("foobar012345", 6);
+    REQUIRE( out.size() == 2u );
+    CHECK( out[0] == "foobar" );
+    CHECK( out[1] == "012345" );
+  }
+  SUBCASE("non-round multi-lines")
+  {
+    const auto out = splitIntoLines("foobar012345xxx", 6);
+    REQUIRE( out.size() == 3u );
+    CHECK( out[0] == "foobar" );
+    CHECK( out[1] == "012345" );
+    CHECK( out[2] == "xxx" );
+  }
+  SUBCASE("sequence is not escaped automatically")
+  {
+    const auto out = splitIntoLines("foo\\bar", 6);
+    REQUIRE( out.size() == 2u );
+    CHECK( out[0] == "foo\\ba");
+    CHECK( out[1] == "r" );
+  }
+  SUBCASE("when escaped EOL is in the line break place, it is moved to a new line")
+  {
+    const auto out = splitIntoLines("foo\\nbar", 4);
+    REQUIRE( out.size() == 3u );
+    CHECK( out[0] == "foo" );
+    CHECK( out[1] == "\\nba" );
+    CHECK( out[2] == "r" );
+  }
+  SUBCASE("when escaped non-EOL is in the line break place, it is moved to a new line")
+  {
+    const auto out = splitIntoLines("foo\\tbar", 4);
+    REQUIRE( out.size() == 3u );
+    CHECK( out[0] == "foo" );
+    CHECK( out[1] == "\\tba" );
+    CHECK( out[2] == "r" );
+  }
+  SUBCASE("when escaped non-printable char is in the line break place, it is moved to a new line")
+  {
+    const auto out = splitIntoLines("foo\\x42bar", 4);
+    REQUIRE( out.size() == 3u );
+    CHECK( out[0] == "foo" );
+    CHECK( out[1] == "\\x42" );
+    CHECK( out[2] == "bar" );
+  }
+  SUBCASE("when escape char is longer than number of columns, longer column is produced to fit the whole sequence")
+  {
+    const auto out = splitIntoLines("foo\\x42bar", 3);
+    REQUIRE( out.size() == 3u );
+    CHECK( out[0] == "foo" );
+    CHECK( out[1] == "\\x42" );
+    CHECK( out[2] == "bar" );
+  }
+  SUBCASE("EOL in last line also breaks line")
+  {
+    const auto out = splitIntoLines("foobar012345x\\nok", 6);
+    REQUIRE( out.size() == 4u );
+    CHECK( out[0] == "foobar" );
+    CHECK( out[1] == "012345" );
+    CHECK( out[2] == "x\\n" );
+    CHECK( out[3] == "ok" );
+  }
 }
 
 
-TEST_CASE("")
+TEST_CASE("wrapping lines around words, when possible")
 {
-  // TODO
-}
-
-
-TEST_CASE("")
-{
-  // TODO
-}
-
-
-TEST_CASE("")
-{
-  // TODO
-}
-
-
-TEST_CASE("")
-{
-  // TODO
-}
-
-
-TEST_CASE("")
-{
-  // TODO
-}
-
-
-TEST_CASE("")
-{
-  // TODO
+  SUBCASE("wrap aroud space")
+  {
+    const auto out = splitIntoLines("foo bar", 6);
+    REQUIRE( out.size() == 2u );
+    CHECK( out[0] == "foo " );
+    CHECK( out[1] == "bar" );
+  }
+  SUBCASE("wrap aroud tab")
+  {
+    const auto out = splitIntoLines("foo\\tbar", 6);
+    REQUIRE( out.size() == 2u );
+    CHECK( out[0] == "foo\\t" );
+    CHECK( out[1] == "bar" );
+  }
+  SUBCASE("wrap aroud CR")
+  {
+    const auto out = splitIntoLines("foo\\nbar", 6);
+    REQUIRE( out.size() == 2u );
+    CHECK( out[0] == "foo\\n" );
+    CHECK( out[1] == "bar" );
+  }
+  SUBCASE("wrap aroud LF")
+  {
+    const auto out = splitIntoLines("foo\\rbar", 6);
+    REQUIRE( out.size() == 2u );
+    CHECK( out[0] == "foo\\r" );
+    CHECK( out[1] == "bar" );
+  }
+  SUBCASE("wrap aroud CR+FL is considered single new line")
+  {
+    const auto out = splitIntoLines("foo\\n\\rbar", 6);
+    REQUIRE( out.size() == 2u );
+    CHECK( out[0] == "foo\\n\\r" );
+    CHECK( out[1] == "bar" );
+  }
+  SUBCASE("wrap aroud FL+CR is considered single new line")
+  {
+    const auto out = splitIntoLines("foo\\r\\nbar", 6);
+    REQUIRE( out.size() == 2u );
+    CHECK( out[0] == "foo\\r\\n" );
+    CHECK( out[1] == "bar" );
+  }
+  SUBCASE("wrapping multiple CRs are considered separate lines")
+  {
+    const auto out = splitIntoLines("foo\\n\\nbar", 7);
+    REQUIRE( out.size() == 3u );
+    CHECK( out[0] == "foo\\n" );
+    CHECK( out[1] == "\\n" );
+    CHECK( out[2] == "bar" );
+  }
+  SUBCASE("wrapping multiple LFs are considered separate lines")
+  {
+    const auto out = splitIntoLines("foo\\r\\rbar", 6);
+    REQUIRE( out.size() == 3u );
+    CHECK( out[0] == "foo\\r" );
+    CHECK( out[1] == "\\r" );
+    CHECK( out[2] == "bar" );
+  }
+  SUBCASE("line of some white spaces")
+  {
+    const auto out = splitIntoLines("a      z", 6);
+    REQUIRE( out.size() == 3u );
+    CHECK( out[0] == "a" );
+    CHECK( out[1] == "      " );
+    CHECK( out[2] == "z" );
+  }
+  SUBCASE("too many white spaces not possible to break")
+  {
+    const auto out = splitIntoLines("a      z", 6);
+    REQUIRE( out.size() == 2u );
+    CHECK( out[0] == "a     " );
+    CHECK( out[1] == " z" );
+  }
 }
 
 }
