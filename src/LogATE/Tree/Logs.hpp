187bfa55a1569db4415aac6f4d33985eb890e272
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-05-20 09:21:04 +0200

    Tree::Logs header re-formatted for more compact style

--- src/LogATE/Tree/Logs.hpp
+++ src/LogATE/Tree/Logs.hpp
@@ -14,51 +14,16 @@ class Logs: public But::Threading::BasicLockable<Logs>,
             public But::Threading::LockProxyProvider<Logs>
 {
 public:
-  auto size() const
-  {
-    BUT_ASSERT( locked() );
-    return logs_.size();
-  }
-  auto empty() const
-  {
-    BUT_ASSERT( locked() );
-    return size() == 0u;
-  }
+  auto size() const { BUT_ASSERT( locked() ); return logs_.size(); }
+  auto empty() const { BUT_ASSERT( locked() ); return size() == 0u; }
 
-  Log const& first() const
-  {
-    BUT_ASSERT( locked() );
-    BUT_ASSERT( not empty() );
-    return logs_.front();
-  }
+  Log const& first() const { BUT_ASSERT( locked() ); BUT_ASSERT( not empty() ); return logs_.front(); }
+  Log const& last()  const { BUT_ASSERT( locked() ); BUT_ASSERT( not empty() ); return logs_.back(); }
 
-  Log const& last() const
-  {
-    BUT_ASSERT( locked() );
-    BUT_ASSERT( not empty() );
-    return logs_.back();
-  }
-
-  auto begin()
-  {
-    BUT_ASSERT( locked() );
-    return logs_.begin();
-  }
-  auto end()
-  {
-    BUT_ASSERT( locked() );
-    return logs_.end();
-  }
-  auto begin() const
-  {
-    BUT_ASSERT( locked() );
-    return logs_.begin();
-  }
-  auto end() const
-  {
-    BUT_ASSERT( locked() );
-    return logs_.end();
-  }
+  auto begin()       { BUT_ASSERT( locked() ); return logs_.begin(); }
+  auto end()         { BUT_ASSERT( locked() ); return logs_.end(); }
+  auto begin() const { BUT_ASSERT( locked() ); return logs_.begin(); }
+  auto end()   const { BUT_ASSERT( locked() ); return logs_.end(); }
 
   std::vector<Log>::iterator find(SequenceNumber sn);
   std::vector<Log>::const_iterator find(SequenceNumber sn) const;
