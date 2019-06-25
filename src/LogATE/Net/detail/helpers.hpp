5b52ba8b99145089bfe773d4b9467788d2f9fab4
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-06-25 13:37:31 +0200

    CG updated

--- src/LogATE/Net/detail/helpers.hpp
+++ src/LogATE/Net/detail/helpers.hpp
@@ -20,7 +20,8 @@ auto sysCallWrapper(F&& f)
   {
     errno = 0;
     tmp = f();
-  } while (tmp == -1 && errno == EINTR);
+  }
+  while(tmp == -1 && errno == EINTR);
   return tmp;
 }
 
@@ -28,44 +29,37 @@ BUT_DEFINE_EXCEPTION(SocketSetupError, Exception, "fcntl() failed");
 
 inline auto fcntlGet(const int fd)
 {
-  const auto flags = sysCallWrapper([&] { return ::fcntl(fd, F_GETFL, 0); });
-  if (flags == -1)
+  const auto flags = sysCallWrapper( [&]{ return ::fcntl(fd, F_GETFL, 0); } );
+  if(flags == -1)
     BUT_THROW(SocketSetupError, "getting options: " << strerror(errno));
   return flags;
 }
 
 inline void fcntlSet(const int fd, const int flags)
 {
-  if (sysCallWrapper([&] { return ::fcntl(fd, F_SETFL, flags); }) == -1)
+  if( sysCallWrapper( [&]{ return ::fcntl(fd, F_SETFL, flags); } ) == -1 )
     BUT_THROW(SocketSetupError, "setting options: " << strerror(errno));
 }
-
-inline void makeNonBlocking(const int fd)
-{
-  fcntlSet(fd, fcntlGet(fd) | O_NONBLOCK);
-}
-inline void makeBlocking(const int fd)
-{
-  fcntlSet(fd, fcntlGet(fd) & ~O_NONBLOCK);
-}
+inline void makeNonBlocking(const int fd) { fcntlSet(fd, fcntlGet(fd) |  O_NONBLOCK); }
+inline void makeBlocking(const int fd)    { fcntlSet(fd, fcntlGet(fd) & ~O_NONBLOCK); }
 
 inline void allowAddressReuse(const int fd)
 {
-  if (sysCallWrapper([=] { const auto flag = 1; return ::setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag)); }) == -1)
+  if( sysCallWrapper( [=]{ const auto flag = 1; return ::setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag)); } ) == -1 )
     BUT_THROW(SocketSetupError, "setsockopt(SO_REUSEPORT): " << strerror(errno));
 }
 
 inline void ignoreDataFromSocket(const int fd)
 {
   char junk[4*1024];
-  while (sysCallWrapper([&] { return ::read(fd, junk, sizeof(junk)); }) > 0)
+  while( sysCallWrapper( [&]{ return ::read(fd, junk, sizeof(junk)); } ) > 0 )
   { }
 }
 
 inline bool waitForData(But::System::Descriptor& interruptSource, const ReadyFor op, But::System::Descriptor& socket)
 {
-  const auto fd = ::LogATE::Net::epoll({{&interruptSource, ReadyFor::Read}, {&socket, op}});
-  if (fd != &interruptSource)
+  const auto fd = ::LogATE::Net::epoll( { {&interruptSource, ReadyFor::Read}, {&socket, op} } );
+  if(fd != &interruptSource)
     return true;
   ignoreDataFromSocket(interruptSource.get());
   return false;
