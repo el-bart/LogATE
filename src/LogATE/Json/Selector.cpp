6b1662c14ddeabe6633f633fac0f33e8c02efee7
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-06-25 16:47:48 +0200

    update() is now inline - this speed things up a bit

--- src/LogATE/Json/Selector.cpp
+++ src/LogATE/Json/Selector.cpp
@@ -5,69 +5,6 @@
 namespace LogATE::Json
 {
 
-namespace
-{
-auto isWhitespace(const char c)
-{
-  return c == ' ' || c == '\r' || c == '\n' || c == '\t';
-}
-}
-
-
-void Selector::update(const char c)
-{
-  if(isInString_)
-  {
-    buffer_.push_back(c);
-    if(passThroughNextChar_)
-    {
-      passThroughNextChar_ = false;
-      return;
-    }
-    if(c=='"')
-    {
-      BUT_ASSERT(not passThroughNextChar_);
-      isInString_ = false;
-      return;
-    }
-    if(c=='\\')
-    {
-      passThroughNextChar_ = true;
-      return;
-    }
-    return;
-  }
-
-  if( isWhitespace(c) )
-    return;
-
-  buffer_.push_back(c);
-
-  switch(c)
-  {
-    case '"':
-         isInString_ = true;
-         return;
-    case '{':
-         ++nestedObjects_;
-         return;
-    case '}':
-         if( nestedObjects_ == 0 )
-           BUT_THROW(UnexpectedCharacter, "trying to end object that was not opened");
-         --nestedObjects_;
-         return;
-    case '[':
-         ++nestedArrays_;
-         return;
-    case ']':
-         if( nestedArrays_ == 0 )
-           BUT_THROW(UnexpectedCharacter, "trying to end array that was not opened");
-         --nestedArrays_;
-         return;
-  }
-}
-
-
 void Selector::eos()
 {
   if( buffer_.empty() )
--- src/LogATE/Json/Selector.hpp
+++ src/LogATE/Json/Selector.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <But/Exception.hpp>
+#include <But/assert.hpp>
 #include <stack>
 #include <vector>
 #include <string>
@@ -17,14 +18,72 @@ struct Selector final
   void eos();
   void reset();
   auto jsonComplete() const { return nestedObjects_ == 0 && nestedArrays_ == 0 && not buffer_.empty() && not isInString_; }
-  auto str() const { return std::string{ begin(buffer_), end(buffer_) }; }
+  auto str() const { return std::string_view{ buffer_.data(), buffer_.size() }; }
 
 private:
+  bool isWhitespace(const char c) const
+  {
+    return c == ' ' || c == '\r' || c == '\n' || c == '\t';
+  }
   bool isInString_{false};
   bool passThroughNextChar_{false};
   uint32_t nestedObjects_{0};
   uint32_t nestedArrays_{0};
   std::string buffer_;
 };
+
+
+inline void Selector::update(const char c)
+{
+  if(isInString_)
+  {
+    buffer_.push_back(c);
+    if(passThroughNextChar_)
+    {
+      passThroughNextChar_ = false;
+      return;
+    }
+    if(c=='"')
+    {
+      BUT_ASSERT(not passThroughNextChar_);
+      isInString_ = false;
+      return;
+    }
+    if(c=='\\')
+    {
+      passThroughNextChar_ = true;
+      return;
+    }
+    return;
+  }
+
+  if( isWhitespace(c) )
+    return;
+
+  buffer_.push_back(c);
+
+  switch(c)
+  {
+    case '"':
+      isInString_ = true;
+      return;
+    case '{':
+      ++nestedObjects_;
+      return;
+    case '}':
+      if( nestedObjects_ == 0 )
+        BUT_THROW(UnexpectedCharacter, "trying to end object that was not opened");
+      --nestedObjects_;
+      return;
+    case '[':
+      ++nestedArrays_;
+      return;
+    case ']':
+      if( nestedArrays_ == 0 )
+        BUT_THROW(UnexpectedCharacter, "trying to end array that was not opened");
+      --nestedArrays_;
+      return;
+  }
+}
 
 }
