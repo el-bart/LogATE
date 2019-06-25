c5b1568b0bcecfdb03c04f3d1c8722a5ac597b1a
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-06-25 12:55:16 +0200

    Json::Selector added to TCP server

--- src/LogATE/Net/TcpServer.hpp
+++ src/LogATE/Net/TcpServer.hpp
@@ -3,6 +3,7 @@
 #include "LogATE/Net/Port.hpp"
 #include "LogATE/Net/Server.hpp"
 #include "LogATE/Utils/WorkerThreads.hpp"
+#include "LogATE/Json/Selector.hpp"
 #include <But/Threading/JoiningThread.hpp>
 #include <Poco/Net/ServerSocket.h>
 #include <Poco/Net/StreamSocket.h>
@@ -36,6 +37,7 @@ private:
   const Poco::Timespan pollTimeout_;
   std::atomic<size_t> errors_{0};
   std::atomic<bool> quit_{false};
+  Json::Selector selector_;
   Queue queue_{2'000};
   Utils::WorkerThreadsShPtr workers_;
   Poco::Net::ServerSocket ss_;
