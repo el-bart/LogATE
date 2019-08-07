d0b17cc81c9b71db8b34d81509af6d122fc0e3d1
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-08-07 21:36:59 +0200

    slight core readability improvement + added random data test

--- src/CursATE/Screen/detail/LogIdIndexCache.ut.cpp
+++ src/CursATE/Screen/detail/LogIdIndexCache.ut.cpp
@@ -3,6 +3,7 @@
 #include "LogATE/Tree/Filter/AcceptAll.hpp"
 #include "LogATE/TestHelpers.ut.hpp"
 #include <sstream>
+#include <random>
 
 using LogATE::makeKey;
 
@@ -13,16 +14,17 @@ TEST_SUITE("CursATE::Screen::detail::LogIdIndexCache")
 
 struct Fixture
 {
-  auto makeLog(unsigned sn, std::string key) const { return LogATE::Log{ makeKey(sn, std::move(key)), "{}" }; }
+  static auto makeLog(unsigned sn, std::string key) { return LogATE::Log{ makeKey(sn, std::move(key)), "{}" }; }
+  static auto makeAnnotatedLog(unsigned sn, std::string key) { return LogATE::AnnotatedLog( makeLog(sn, std::move(key)) ); }
 
   void fillWithData()
   {
-    node_->insert( LogATE::AnnotatedLog{ makeLog(1, "bar") } );
-    node_->insert( LogATE::AnnotatedLog{ makeLog(4, "bar") } );
-    node_->insert( LogATE::AnnotatedLog{ makeLog(0, "foo") } );
-    node_->insert( LogATE::AnnotatedLog{ makeLog(8, "kamboom!") } );
-    node_->insert( LogATE::AnnotatedLog{ makeLog(5, "kszy") } );
-    node_->insert( LogATE::AnnotatedLog{ makeLog(3, "narf") } );
+    node_->insert( makeAnnotatedLog(1, "bar") );
+    node_->insert( makeAnnotatedLog(4, "bar") );
+    node_->insert( makeAnnotatedLog(0, "foo") );
+    node_->insert( makeAnnotatedLog(8, "kamboom!") );
+    node_->insert( makeAnnotatedLog(5, "kszy") );
+    node_->insert( makeAnnotatedLog(3, "narf") );
   }
 
   LogATE::Utils::WorkerThreadsShPtr workers_{ But::makeSharedNN<LogATE::Utils::WorkerThreads>() };
@@ -107,7 +109,7 @@ TEST_CASE_FIXTURE(Fixture, "auto-cleanup of outdated entries")
 
 TEST_CASE_FIXTURE(Fixture, "test working on 1-element set")
 {
-  node_->insert( LogATE::AnnotatedLog{ makeLog(0, "foo") } );
+  node_->insert( makeAnnotatedLog(0, "foo") );
   CHECK( liic_.size() == 0u );
   CHECK( liic_.index( makeKey(1, "bar") ) == 0 );
   CHECK( liic_.size() == 0u );
@@ -164,7 +166,49 @@ TEST_CASE_FIXTURE(Fixture, "test random scenarios")
   }
 }
 
-// TODO: pseudo-random search in a big data set
+
+auto randomData()
+{
+  std::vector<LogATE::AnnotatedLog> out;
+  const auto alphabet = {0,1,2,3,4,5,6,7,8,9};
+  auto counter = 0u;
+  for(auto i: alphabet)
+    for(auto j: alphabet)
+      for(auto k: alphabet)
+          out.push_back( Fixture::makeAnnotatedLog( counter++, std::to_string(i) + std::to_string(j) + std::to_string(k) ) );
+  return out;
+}
+
+auto randomQueries(std::vector<LogATE::AnnotatedLog> const& in, unsigned seed)
+{
+  std::vector<std::pair<LogATE::Log::Key, size_t>> out;
+  out.reserve( in.size() );
+  auto pos = 0u;
+  for(auto& e: in)
+    out.push_back( std::make_pair( e.key(), pos++ ) );
+  std::mt19937_64 prng{seed};
+  std::shuffle( begin(out), end(out), prng );
+  return out;
+}
+
+TEST_CASE_FIXTURE(Fixture, "random scenarios - big data sets")
+{
+  const auto data = randomData();
+  for(auto& e: data)
+    node_->insert(e);
+  CHECK( liic_.size() == 0u );
+
+  SUBCASE("order 1")
+  {
+    for(auto& q: randomQueries(data, 40+1))
+      CHECK( liic_.index(q.first) == q.second );
+  }
+  SUBCASE("order 2")
+  {
+    for(auto& q: randomQueries(data, 40+2))
+      CHECK( liic_.index(q.first) == q.second );
+  }
+}
 
 }
 }
