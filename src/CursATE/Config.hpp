732e0d0ef7470fb8b6c2e2e3eeeabd96a1a49a45
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-08-14 11:53:39 +0200

    only port and parse mode currentl have defaults

--- src/CursATE/Config.hpp
+++ src/CursATE/Config.hpp
@@ -9,10 +9,10 @@ namespace CursATE
 struct Config final
 {
   LogATE::Net::Port port_{4242};
-  LogATE::Net::TcpServer::JsonParsingMode jsonParsingMode_{ LogATE::Net::TcpServer::JsonParsingMode::ParseToEndOfJson };
-  LogATE::Tree::Path keyPath_{ LogATE::Tree::Path::parse(".But::PreciseDT") };
-  LogATE::Printers::OrderedPrettyPrint::SilentTags silentTags_{{"But::PreciseDT", "Priority", "ComponentId", "UniqueId", "string"}};
-  LogATE::Printers::OrderedPrettyPrint::PriorityTags priorityTags_{{"But::PreciseDT", "Priority", "ComponentId", "UniqueId", "But::ThreadNo", "string"}};
+  LogATE::Net::TcpServer::JsonParsingMode jsonParsingMode_{LogATE::Net::TcpServer::JsonParsingMode::HardBreakOnNewLine};
+  LogATE::Tree::Path keyPath_;
+  LogATE::Printers::OrderedPrettyPrint::SilentTags silentTags_;
+  LogATE::Printers::OrderedPrettyPrint::PriorityTags priorityTags_;
 };
 
 }
