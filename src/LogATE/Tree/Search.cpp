14b386852f3fe80f71ea0f6326e80619f6107a52
bartek 'basz' szurgot <bartosz.szurgot@qiagen.com>
2019-10-09 14:07:17 +0200

    parallel search engine implemented

--- src/LogATE/Tree/Search.cpp
+++ src/LogATE/Tree/Search.cpp
@@ -1,4 +1,8 @@
 #include "LogATE/Tree/Search.hpp"
+#include "But/Threading/LockProxyProvider.hpp"
+#include "But/Threading/BasicLockable.hpp"
+#include "But/Guard.hpp"
+#include "But/assert.hpp"
 
 namespace LogATE::Tree
 {
@@ -9,15 +13,176 @@ Search::Search(Utils::WorkerThreadsShPtr workers, const uint64_t chunkSize):
 { }
 
 
-Search::Result Search::search(LogsPtr logs, Log::Key startPoint, Direction dir, Query q)
+namespace
 {
-  (void)logs;
-  (void)startPoint;
-  (void)dir;
-  (void)q;
-  (void)chunkSize_;
-  (void)workers_;
-  throw 42;
+auto getChunk(Search::LogsPtr const& logs, Log::Key const& startPoint, const Search::Direction dir, const uint64_t chunkSize)
+{
+  switch(dir)
+  {
+    case Search::Direction::Forward:  return logs->withLock()->from(startPoint, chunkSize);
+    case Search::Direction::Backward: return logs->withLock()->to  (startPoint, chunkSize);
+  }
+  BUT_ASSERT(!"invalid direction requested");
+  throw std::logic_error{"invalid direction"};
+}
+
+
+But::Optional<Log::Key> lastKey(std::vector<Log> const& logs, const Search::Direction dir)
+{
+  if( logs.empty() )
+    return {};
+  switch(dir)
+  {
+    case Search::Direction::Forward:  return logs.back().key();
+    case Search::Direction::Backward: return logs.front().key();
+  }
+  BUT_ASSERT(!"invalid direction requested");
+  throw std::logic_error{"invalid direction"};
+}
+
+
+struct SharedState: But::Threading::BasicLockable<SharedState>,
+                    But::Threading::LockProxyProvider<SharedState>
+{
+  explicit SharedState(But::NotNullShared<std::atomic<bool>> cancel): cancel_{ std::move(cancel) } { }
+
+  But::NotNullShared<std::atomic<bool>> cancel_;
+  But::Optional<Log::Key> keyFound_{};
+  std::atomic<uint64_t> totalTasks_{0};
+  std::atomic<uint64_t> doneTasks_{0};
+};
+
+
+struct ForwardSearchJob
+{
+  void operator()()
+  {
+    const auto taskGuard = But::makeGuard( [s=state_] { ++s->doneTasks_; } );
+    for(auto& e: chunk_)
+    {
+      try
+      {
+        if(*state_->cancel_)
+          return;
+        if( not query_(e) )
+          continue;
+        reportFinding( e.key() );
+        return;
+      }
+      catch(...)
+      { }
+    }
+  }
+
+  void reportFinding(Log::Key const& key)
+  {
+    auto ls = state_->withLock();
+    if(not ls->keyFound_)
+    {
+      ls->keyFound_ = key;
+      return;
+    }
+    if( key < *ls->keyFound_ )
+    {
+      ls->keyFound_ = key;
+      return;
+    }
+  }
+
+  Search::Query query_;
+  But::NotNullShared<SharedState> state_;
+  std::vector<Log> chunk_;
+};
+
+
+struct BackwardSearchJob
+{
+  void operator()()
+  {
+    const auto taskGuard = But::makeGuard( [s=state_] { ++s->doneTasks_; } );
+    for(auto it=chunk_.rbegin(); it!=chunk_.rend(); ++it)
+    {
+      try
+      {
+        if(*state_->cancel_)
+          return;
+        if( not query_(*it) )
+          continue;
+        reportFinding( it->key() );
+        return;
+      }
+      catch(...)
+      { }
+    }
+  }
+
+  void reportFinding(Log::Key const& key)
+  {
+    auto ls = state_->withLock();
+    if(not ls->keyFound_)
+    {
+      ls->keyFound_ = key;
+      return;
+    }
+    if( *ls->keyFound_ < key )
+    {
+      ls->keyFound_ = key;
+      return;
+    }
+  }
+
+  Search::Query query_;
+  But::NotNullShared<SharedState> state_;
+  std::vector<Log> chunk_;
+};
+
+
+struct SetSearchResult
+{
+  void operator()()
+  {
+    while( state_->totalTasks_ != state_->doneTasks_ && not state_->cancel_->load() )
+      std::this_thread::yield();
+    if( not state_->cancel_->load() )
+      promise_->set_value( std::move(state_->keyFound_) );
+    else
+      promise_->set_value( But::Optional<Log::Key>{} );
+  }
+
+  But::NotNullShared<SharedState> state_;
+  But::NotNullShared<std::promise<But::Optional<Log::Key>>> promise_;
+};
+}
+
+
+Search::Result Search::search(const LogsPtr logs, const Log::Key startPoint, const Direction dir, const Query q)
+{
+  auto cancel = But::makeSharedNN<std::atomic<bool>>(false);
+  auto promise = But::makeSharedNN<std::promise<But::Optional<Log::Key>>>();
+  Search::Result result{ promise->get_future(), cancel };
+  auto state = But::makeSharedNN<SharedState>(cancel);
+
+  But::Optional<Log::Key> keyNow = startPoint;
+  while(keyNow)
+  {
+    auto chunk = getChunk(logs, *keyNow, dir, chunkSize_);
+    keyNow = lastKey(chunk, dir);
+    const auto size = chunk.size();
+    switch(dir)
+    {
+      case Search::Direction::Forward:  workers_->enqueueUi( ForwardSearchJob{  q, state, std::move(chunk) } ); break;
+      case Search::Direction::Backward: workers_->enqueueUi( BackwardSearchJob{ q, state, std::move(chunk) } ); break;
+    }
+    ++state->totalTasks_;
+    if(size < chunkSize_)
+    {
+      keyNow.reset();
+      break;
+    }
+  }
+
+  workers_->enqueueUi( SetSearchResult{ state, std::move(promise) } );
+  return result;
 }
 
 }
--- src/LogATE/Tree/Search.ut.cpp
+++ src/LogATE/Tree/Search.ut.cpp
@@ -1,4 +1,5 @@
 #include <doctest/doctest.h>
+#include "But/Threading/Event.hpp"
 #include "LogATE/Tree/Search.hpp"
 #include "LogATE/TestHelpers.ut.hpp"
 #include "LogATE/TestPrints.ut.hpp"
@@ -6,6 +7,7 @@
 using LogATE::Tree::Search;
 using LogATE::Tree::Logs;
 using LogATE::Log;
+
 
 namespace
 {
@@ -18,85 +20,270 @@ struct Fixture
   {
     std::vector<Log> out;
     out.reserve( ns.size() );
+    auto counter = 0u;
     for(auto e: ns)
     {
       auto json = nlohmann::json::object();
       json["answer"] = e;
-      auto log = LogATE::makeLog( json.dump() );
+      auto log = LogATE::makeLog( counter++, json.dump() );
       out.push_back(log);
       logs_->withLock()->insert( std::move(log) );
     }
     return out;
   }
 
-  LogATE::Utils::WorkerThreadsShPtr workers_{ But::makeSharedNN<LogATE::Utils::WorkerThreads>() };
+  auto waitFor(Search::Result& res) const
+  {
+    return res.value_.wait_for(timeout_) == std::future_status::ready;
+  }
+
+  LogATE::Utils::WorkerThreadsShPtr workers_{ But::makeSharedNN<LogATE::Utils::WorkerThreads>(3) };
   But::NotNullShared<Logs> logs_{ But::makeSharedNN<Logs>() };
-  Search s{workers_, 5};
+  Search s_{workers_, 5};
+  const Search::Direction forward_{Search::Direction::Forward};
+  const Search::Direction backward_{Search::Direction::Backward};
+  const std::chrono::seconds timeout_{42};
+};
+
+
+struct LookForAnswer
+{
+  bool operator()(Log const& log) const
+  {
+    return log.json()["answer"].get<int>() == n_;
+  }
+
+  int n_{0};
 };
 
 
 TEST_CASE_FIXTURE(Fixture, "searching within one block")
 {
+  const auto data = populate({1,2,3,4,5});
   SUBCASE("forward")
   {
+    auto result = s_.search( logs_, data[0].key(), forward_, LookForAnswer{4} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(opt);
+    CHECK( *opt == data[3].key() );
   }
   SUBCASE("backward")
   {
+    auto result = s_.search( logs_, data[4].key(), backward_, LookForAnswer{4} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(opt);
+    CHECK( *opt == data[3].key() );
+  }
+}
+
+
+TEST_CASE_FIXTURE(Fixture, "start element is included in the search")
+{
+  const auto data = populate({1,2,3,4,5});
+  SUBCASE("forward")
+  {
+    auto result = s_.search( logs_, data[3].key(), forward_, LookForAnswer{4} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(opt);
+    CHECK( *opt == data[3].key() );
+  }
+  SUBCASE("backward")
+  {
+    auto result = s_.search( logs_, data[3].key(), backward_, LookForAnswer{4} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(opt);
+    CHECK( *opt == data[3].key() );
   }
 }
 
 
 TEST_CASE_FIXTURE(Fixture, "searching within multiple blocks")
 {
+  const auto data = populate({1,2,3,4,5, 1,2,3,4,5, 6,7,8,9,10});
   SUBCASE("forward")
   {
+    auto result = s_.search( logs_, data[0].key(), forward_, LookForAnswer{4} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(opt);
+    CHECK( *opt == data[3].key() );
   }
   SUBCASE("backward")
   {
+    auto result = s_.search( logs_, data[14].key(), backward_, LookForAnswer{4} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(opt);
+    CHECK( *opt == data[8].key() );
   }
 }
 
+
+struct BlockingQuery
+{
+  bool operator()(Log const&) const
+  {
+    waitForStart_->set();
+    REQUIRE( unleash_->wait(timeout_) );
+    return true;
+  }
+
+  std::chrono::seconds timeout_;
+  But::NotNullShared<But::Threading::Event> waitForStart_;
+  But::NotNullShared<But::Threading::Event> unleash_;
+};
 
 TEST_CASE_FIXTURE(Fixture, "canceling search")
 {
+  REQUIRE( workers_->threads() == 3 );
+  const auto data = populate({1,2,3,4,5, 1,2,3,4,5, 6,7,8,9,10});
+  auto waitForStart = But::makeSharedNN<But::Threading::Event>();
+  auto unleash = But::makeSharedNN<But::Threading::Event>();
+  const BlockingQuery query{timeout_, waitForStart, unleash};
   SUBCASE("forward")
   {
+    auto result = s_.search( logs_, data[0].key(), forward_, query);
+    REQUIRE( waitForStart->wait(timeout_) );
+    *result.cancel_ = true;
+    unleash->set();
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(not opt);
   }
   SUBCASE("backward")
   {
-  }
-}
-
-
-TEST_CASE_FIXTURE(Fixture, "searching for a non-existing element")
-{
-  SUBCASE("forward")
-  {
-  }
-  SUBCASE("backward")
-  {
+    auto result = s_.search( logs_, data[14].key(), backward_, query);
+    REQUIRE( waitForStart->wait(timeout_) );
+    *result.cancel_ = true;
+    unleash->set();
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(not opt);
   }
 }
 
 
 TEST_CASE_FIXTURE(Fixture, "searcing with multiple matches returns first one")
 {
+  const auto data = populate({1,2,3,2,1});
   SUBCASE("forward")
   {
+    auto result = s_.search( logs_, data[0].key(), forward_, LookForAnswer{2} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(opt);
+    CHECK( *opt == data[1].key() );
   }
   SUBCASE("backward")
   {
+    auto result = s_.search( logs_, data[4].key(), backward_, LookForAnswer{2} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(opt);
+    CHECK( *opt == data[3].key() );
   }
 }
 
 
+struct ThrowOnceAndLookForAnswer
+{
+  explicit ThrowOnceAndLookForAnswer(int n): lfa_{n} { }
+
+  bool operator()(Log const& log)
+  {
+    if(not alreadyThrown_)
+    {
+      alreadyThrown_ = true;
+      throw std::runtime_error{"some expected error"};
+    };
+    return lfa_(log);
+  }
+
+  bool alreadyThrown_{false};
+  LookForAnswer lfa_;
+};
+
 TEST_CASE_FIXTURE(Fixture, "exception in query does not break the search")
 {
+  const auto data = populate({1,2,3,4,5});
   SUBCASE("forward")
   {
+    auto result = s_.search( logs_, data[0].key(), forward_, ThrowOnceAndLookForAnswer{3} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(opt);
+    CHECK( *opt == data[2].key() );
   }
   SUBCASE("backward")
   {
+    auto result = s_.search( logs_, data[4].key(), backward_, ThrowOnceAndLookForAnswer{3} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(opt);
+    CHECK( *opt == data[2].key() );
+  }
+}
+
+
+TEST_CASE_FIXTURE(Fixture, "non-existing elements are not found")
+{
+  const auto data = populate({1,2,3,4,5});
+  SUBCASE("forward")
+  {
+    auto result = s_.search( logs_, data[2].key(), forward_, LookForAnswer{42} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(not opt);
+  }
+  SUBCASE("backward")
+  {
+    auto result = s_.search( logs_, data[2].key(), backward_, LookForAnswer{42} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(not opt);
+  }
+}
+
+
+TEST_CASE_FIXTURE(Fixture, "elements not in range are not found")
+{
+  const auto data = populate({1,2,3,4,5});
+  SUBCASE("forward")
+  {
+    auto result = s_.search( logs_, data[2].key(), forward_, LookForAnswer{2} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(not opt);
+  }
+  SUBCASE("backward")
+  {
+    auto result = s_.search( logs_, data[2].key(), backward_, LookForAnswer{4} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(not opt);
+  }
+}
+
+
+TEST_CASE_FIXTURE(Fixture, "searching empty set does not return anything")
+{
+  SUBCASE("forward")
+  {
+    auto result = s_.search( logs_, LogATE::makeKey(1), forward_, LookForAnswer{2} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(not opt);
+  }
+  SUBCASE("backward")
+  {
+    auto result = s_.search( logs_, LogATE::makeKey(1), backward_, LookForAnswer{2} );
+    REQUIRE( waitFor(result) );
+    const auto opt = result.value_.get();
+    REQUIRE(not opt);
   }
 }
 
