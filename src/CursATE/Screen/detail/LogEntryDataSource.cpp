cedb796474b778f417dbbab474f74216847c88b2
bartek 'basz' szurgot <git@baszerr.eu>
2022-06-20 21:48:58 +0200

    LogEntryDataSource now generates proper paths and readable keys

--- src/CursATE/Screen/detail/LogEntryDataSource.cpp
+++ src/CursATE/Screen/detail/LogEntryDataSource.cpp
@@ -76,39 +76,114 @@ But::Optional<std::string> LogEntryDataSource::id2value(Id const& id) const
 
 namespace
 {
-But::Optional<std::string> optPrintable(PrintableStringConverter const& printable, But::Optional<std::string> const& in)
+But::Optional<std::string> optPrintable(PrintableStringConverter const& printable, But::Optional<std::string> in)
 {
   if(not in)
     return {};
   return printable(*in);
 }
 
+auto indent(std::string const& prefix) { return prefix + "  "; }
+
+
+auto extend(Path const& path, std::string key)
+{
+  auto newPathData = path.data();
+  newPathData.emplace_back( std::move(key) );
+  return Path{ std::move(newPathData), path.isAbsolute() };
+}
+
+
+auto extend(Path const& path, std::string const& key, size_t index)
+{
+  std::stringstream ss;
+  ss << key << '[' << index << ']';
+  return extend( path, ss.str() );
+}
+
+
+template<typename C>
+void appendTree(nlohmann::json const& log, C& out, Path const& path, PrintableStringConverter const& printable, std::string const& prefix);
+
+
+template<typename C>
+void appendTreeObject(nlohmann::json const& log, C& out, Path const& path, PrintableStringConverter const& printable, std::string const& prefix)
+{
+  BUT_ASSERT( log.is_object() );
+
+  for( auto& [k,v]: log.items() )
+  {
+    std::stringstream ssText;
+    ssText << prefix << printable(k);
+    auto value = value2str(v);  // TODO: optPrintable( v2s() ) ?
+    if(value)
+      ssText << ": " << printable(*value);
+
+    auto newPath = extend( path, printable(k) );
+    using E = typename C::value_type;
+    out.push_back( E{ newPath, ssText.str(), optPrintable(printable, value) } );
+
+    if(not value)
+      appendTree( v, out, std::move(newPath), printable, indent(prefix) );
+  }
+}
+
+
+template<typename C>
+void appendTreeArray(nlohmann::json const& log, C& out, Path const& path, PrintableStringConverter const& printable, std::string const& prefix)
+{
+  BUT_ASSERT( log.is_array() );
+  BUT_ASSERT( not path.empty() && "root-level arrays are not allowed" );
+  auto pathTemplateData = path.data();
+  pathTemplateData.pop_back();
+  auto pathTemplate = Path{ std::move(pathTemplateData), path.isAbsolute() };
+
+  auto index = size_t{0};
+  for(auto& e: log)
+  {
+    std::stringstream ssText;
+    ssText << prefix << '[' << index << ']';
+    auto value = value2str(e);  // TODO: optPrintable ?
+    if(value)
+      ssText << ": " << printable(*value);
+
+    auto newPath = extend(pathTemplate, printable( path.last().name() ), index);
+    using E = typename C::value_type;
+    out.push_back( E{ newPath, ssText.str(), optPrintable(printable, value) } );
+    ++index;
+
+    if(not value)
+      appendTree( e, out, std::move(newPath), printable, indent(prefix) );
+  }
+}
+
+
 template<typename C>
 void appendTree(nlohmann::json const& log, C& out, Path const& path, PrintableStringConverter const& printable, std::string const& prefix)
 {
-  const auto newPrefix = prefix + "  ";
-  using E = typename C::value_type;
-  for( auto& e: log.items() )
+  if( log.is_object() )
   {
-    auto newPathData = path.data();
-    newPathData.emplace_back( printable( e.key() ) );
-    auto value = value2str( e.value() );
-    auto text = prefix + printable( e.key() );
-    if( e.value().is_array() )
-      text += "[]";
-    if(value)
-      text += ": " + printable( *value );
-    out.push_back( E{ Path{newPathData, path.isAbsolute()}, std::move(text), optPrintable(printable, value) } );
-    if(not value)
-      appendTree(e.value(), out, Path{newPathData, path.isAbsolute()}, printable, newPrefix);
+    appendTreeObject(log, out, path, printable, prefix);
+    return;
   }
+
+  if( log.is_array() )
+  {
+    appendTreeArray(log, out, path, printable, prefix);
+    return;
+  }
+
+  BUT_ASSERT("top level element has to be an object");
+  throw std::logic_error{"top level element has to be an object"};
 }
 }
 
 std::vector<LogEntryDataSource::Entry> LogEntryDataSource::constructEntries(LogATE::Log const& log) const
 {
+  if( not log.json().is_object() )
+    throw std::logic_error{"only objects can be root-level elements"};
   std::vector<Entry> out;
-  appendTree(log.json(), out, Path{{"."}}, printable_, "");
+  appendTree( log.json(), out, Path{{"."}}, printable_, "" );
   return out;
 }
 
