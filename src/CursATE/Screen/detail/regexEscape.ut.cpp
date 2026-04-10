cec221fe57a12cffe62b39ecb14b97af4b19e262
bartek 'basz' szurgot <git@baszerr.eu>
2026-04-10 17:18:30 +0200

    added more robust RE tests (checkis if escaped RE is fine, too)

--- src/CursATE/Screen/detail/regexEscape.ut.cpp
+++ src/CursATE/Screen/detail/regexEscape.ut.cpp
@@ -1,5 +1,7 @@
 #include <doctest/doctest.h>
 #include "CursATE/Screen/detail/regexEscape.hpp"
+#include "LogATE/Utils/matchesLog.hpp"
+#include <regex>
 
 using CursATE::Screen::detail::regexEscape;
 
@@ -8,25 +10,45 @@ namespace
 TEST_SUITE("CursATE::Screen::detail::regexEscape")
 {
 
+#define CHECK_ESCAPING(in, exp) \
+	SUBCASE(in) \
+	{ \
+		CHECK( regexEscape(in) == exp ); \
+		\
+		auto const out = regexEscape(in); \
+		try \
+		{ \
+			std::regex const re{"^" + out + "$", LogATE::Utils::g_defaultRegexType}; \
+			CHECK( std::regex_search(in, re) ); \
+		} \
+		catch(std::exception const& ex) \
+		{ \
+			INFO(in); \
+			INFO(out); \
+			FAIL( ("input string generted invalid RE: " + std::string{ex.what()}) ); \
+		} \
+	}
+
 
 TEST_CASE("direct output")
 {
-  CHECK( regexEscape("") == "" );
-  CHECK( regexEscape("abc") == "abc" );
-  CHECK( regexEscape("a/b#c") == "a/b#c" );
-  CHECK( regexEscape("a b\tc\n") == "a b\tc\n" );
+  CHECK_ESCAPING( "", "" );
+  CHECK_ESCAPING( "abc", "abc" );
+  CHECK_ESCAPING( "a/b#c", "a/b#c" );
+  CHECK_ESCAPING( "a b\tc\n", "a b\tc\n" );
 }
 
 
 TEST_CASE("escaping")
 {
-  CHECK( regexEscape(R"([])") == R"(\[\])" );
-  CHECK( regexEscape(R"({})") == R"(\{\})" );
-  CHECK( regexEscape(R"(\)") == R"(\\)" );
-  CHECK( regexEscape(R"(*+?)") == R"(\*\+\?)" );
-  CHECK( regexEscape(R"(.)") == R"(\.)" );
-  CHECK( regexEscape(R"(|)") == R"(\|)" );
-  CHECK( regexEscape(R"(^$)") == R"(\^\$)" );
+  CHECK_ESCAPING( R"([])", R"(\[\])" );
+  CHECK_ESCAPING( R"({})", R"(\{\})" );
+  CHECK_ESCAPING( R"(\)", R"(\\)" );
+  CHECK_ESCAPING( R"(*+?)", R"(\*\+\?)" );
+  CHECK_ESCAPING( R"(.)", R"(\.)" );
+  CHECK_ESCAPING( R"(|)", R"(\|)" );
+  CHECK_ESCAPING( R"(^$)", R"(\^\$)" );
+  CHECK_ESCAPING( R"([foo-bar] xyz...)", R"(\[foo-bar\] xyz\.\.\.)" );
 }
 
 }
